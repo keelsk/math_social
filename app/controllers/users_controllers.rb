2c3246eca0e29fd9fcb7590a37bed9d7ed57fe7b
Kirsten Keels <keelsk@gmail.com>
2020-04-06 20:02:04 +0000

    create signup form and post route for signup

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -3,7 +3,23 @@ class UsersController < ApplicationController
     erb :'users/login'
   end
 
+  post '/login' do
+  end
+
   get '/signup' do
     erb :'users/signup'
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+      flash[:message] = "Please enter a valid username and password"
+    end
+    @user = User.create(params, img_url: generate_image)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/problems'
+    else
+      redirect '/signup'
   end
 end
