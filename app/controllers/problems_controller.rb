6ff2511916aff3ebb5175b3ebce19640ea2dcf17
Kirsten Keels <keelsk@gmail.com>
2020-04-06 22:14:12 +0000

    create homepage for user and add a controller action to show all a users problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,14 @@
 class ProblemsController < ApplicationController
   get "/problem-home" do
-    "hey"
+    redirect "/" if !logged_in?
+    @problems = Problem.all
+    @user = current_user
+    @users = User.all
+    erb :'problems/index'
   end
+
+  get "/:username/problems" do
+
+  end
+
 end
