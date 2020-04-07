75135b92e042c6d07d071a116fb41395e7dd7e57
Kirsten Keels <keelsk@gmail.com>
2020-04-07 22:58:06 +0000

    add index actions and delete actions for friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,14 @@
 class FriendsController < ApplicationController
-  get "/friends/:user" do
+  get "/friends/:username" do
+    @user = User.find_by(username: params[:username])
+    @friends = @user.get_friends
     erb :"friends/index"
+  end
+
+  delete "/friends/:friend_username" do
+    friend = Friend.find_by(username: params[:friend_username])
+    friend.destroy
+    username = current_user.username
+    redirect "/friends/#{username}"
   end
 end
