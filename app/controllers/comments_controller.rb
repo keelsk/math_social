02bc33dbfd24c7db73e751958842fa73b3cd0e78
Kirsten Keels <keelsk@gmail.com>
2020-04-07 20:59:40 +0000

    add edit and delete actions to comment controllers and view pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,39 @@
 class CommentsController < ApplicationController
+  get "/comments/:solution_id" do
+    @solution = Solution.find_by_id(params[:solution_id])
+    @problem = @solution.problem
+    erb :'comments/new'
+  end
+
   post "/comments/:solution_id" do
+    @solution = Solution.find_by_id(params[:solution_id])
+    new_comment = Comment.create(content: params[:content])
+    @solution.comments << new_comment
+    @solution.save
+    user = current_user
+    user.comments  << new_comment
+    user.save
+    redirect "/solutions/#{@solution.id}"
+  end
+
+  get "/comments/:comment_id/edit" do
+    @comment = Comment.find_by_id(params[:comment_id])
+    @solution = @comment.solution
+    @problem = @solution.problem
+    erb :'comments/edit'
+  end
+
+  patch "/comments/:comment_id" do
+    comment = Comment.find_by_id(params[:comment_id])
+    comment.update(content: params[:content])
+    solution = comment.solution
+    redirect "/solutions/#{solution.id}"
+  end
+
+  delete "/comments/:comment_id" do
+    comment = Comment.find_by_id(params[:comment_id])
+    solution = comment.solution
+    comment.destroy
+    redirect "/solutions/#{solution.id}"
   end
 end
