6de01abab935ae559edbbdd7b7be1dd3a685bca8
Kirsten Keels <keelsk@gmail.com>
2020-04-07 01:27:32 +0000

    fix comments on show page in solutions controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,5 +1,9 @@
 class SolutionsController < ApplicationController
   get "/solutions/:id" do
+    @solution = Solution.find_by_id(params[:id])
+    @problem = @solution.problem
+    # @comments = Comment.find_by(solution_id: @solution.id)
+    erb :'solutions/show'
     # id refers to solution id
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,26 @@ problem = "There are 5 candies. Susie ate 3. How many candies are there now?"
 answer = 5-3
 
 problem_1 = Problem.create(question: "There are 5 candies. Susie ate 3. How many candies are there now?", answer: answer)
-
 kirsten.problems << problem_1
-
 kirsten.save
 # problem_1.user = kirsten
 
 
 student_solution = Solution.create(explanation: "I knew I needed to subtract so I did 5-3.", student_answer: 2)
-
 problem_1.solution = student_solution
+problem_1.save
 
-first_comment = Comment.create(content:"I agree with you!")
+first_comment = Comment.create(content: "I agree with you.")
+student_solution.comments << first_comment
+student_solution.save
 
-first_comment.user = kelley
+second_comment = Comment.create(content: "I changed my mind.")
+student_solution.comments << second_comment
+student_solution.save
+# first_comment = Comment.create(content: "I agree with you!")
 
-first_comment.solution = student_solution
+# kelley.comments << first_comment
+# kelley.save
+
+# student_solution.comments << first_comment
+# student_solution.save
